fee0f1e44add66c686c7037271e596bffa071ad9
SadmanSkb55 <119743258+SadmanSkb55@users.noreply.github.com>
2024-03-19 23:47:24 +0600

    Sorted

---
+++ PracticeWhatever/Sorted_List.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <list>
+#include <algorithm>
+
+using namespace std;
+int main() {
+    list<int> mylist = {5, 2, 9, 1, 7}; 
+    
+    
+    mylist.sort(); 
+   cout << "Sorted list: ";
+    for (const auto& elem : mylist) {
+       cout << elem << " ";
+    }
+    cout<<endl;
+    
+    return 0;
+}
